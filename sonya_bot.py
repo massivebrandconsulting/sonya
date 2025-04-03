42aa4dfe35b07e7336ea1f18a2e72063cd51a624
massivebrandconsulting <tanya@massivebrandconsulting.com>
2025-04-03 19:39:34 -0400

    Update sonya_bot.py

--- sonya_bot.py
+++ sonya_bot.py
@@ -55,3 +55,9 @@ def slack_events():
         })
 
     return jsonify({"status": "ok"})
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
